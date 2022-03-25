5dd9754838d03543436a88f7857669f3f4b738b2
xiaossr <63619055+xiaossr@users.noreply.github.com>
2022-03-24 21:32:56 -0500

    Create P.cpp

---
+++ intro/P.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+int main()
+{
+	cin >> N;
+
+	if (N == 1) cout << "1\n";
+	else if (N < 4) cout << "NO SOLUTION\n";
+	else
+	{
+		for (int i = 2; i <= N; i += 2)
+			cout << i << " ";
+		for (int i = 1; i <= N; i += 2)
+			cout << i << " ";
+		cout << "\n";
+	}
+
+	return 0;
+}
