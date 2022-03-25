d2bfb57277cd0f66d6dea166a888577b9308d8ba
xiaossr <63619055+xiaossr@users.noreply.github.com>
2022-03-24 21:01:34 -0500

    Create WA.cpp

---
+++ intro/WA.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+long long N;
+
+int main()
+{
+    cin >> N;
+
+    while (N != 1)
+    {
+        cout << N << " ";
+        if (N % 2) N = 3 * N + 1;
+        else N /= 2;
+    }
+    cout << "1\n";
+
+    return 0;
+}
