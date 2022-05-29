3d4e302c2fd3deaad609af5f5e33f5b6bf74480c
xiaossr <63619055+xiaossr@users.noreply.github.com>
2022-05-28 19:00:43 -0500

    Create RUQ.cpp

---
+++ range-queries/RUQ.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <queue>
+#include <stack>
+#include <set>
+#include <algorithm>
+#include <fstream>
+#include <cstring>
+#include <cmath>
+#include <cassert>
+#include <string>
+#include <map>
+#include <utility>
+#include <iomanip>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef vector<int> vi;
+
+#define pb push_back
+#define INF ((int) 2e9)
+
+const int MOD = 1e9 + 7;
+const int maxN = 2e5 + 5;
+
+int N, Q;
+ll segtree[4 * maxN];
+
+int combine(int a, int b)
+{
+	return a ^ b;
+}
+
+void build(int a[], int v, int tl, int tr)
+{
+	if (tl == tr) segtree[v] = a[tl];
+	else
+	{
+		int tm = (tl + tr) / 2;
+		build(a, v * 2, tl, tm);
+		build(a, v * 2 + 1, tm + 1, tr);
+		segtree[v] = 0;
+		// segtree[v] = combine(segtree[v * 2], segtree[v * 2 + 1]);
+	}
+}
+
+int get_xor(int v, int tl, int tr, int l, int r)
+{
+	if (l > r) return 0;
+	if (l == tl && r == tr) return segtree[v];
+	int tm = (tl + tr) / 2;
+
+	return combine(get_xor(v * 2, tl, tm, l, min(r, tm)),
+		get_xor(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r));
+}
+
+ll get_pt(int v, int tl, int tr, int pos)
+{
+	if (tl == tr) return segtree[v];
+	int tm = (tl + tr) / 2;
+
+	if (pos <= tm)
+		return segtree[v] + get_pt(v * 2, tl, tm, pos);
+	else return segtree[v] + get_pt(v * 2 + 1, tm + 1, tr, pos);
+}
+
+void upd(int v, int tl, int tr, int pos, int nv)
+{
+	if (tl == tr) segtree[v] = nv;
+	else
+	{
+		int tm = (tl + tr) / 2;
+		if (pos <= tm)
+			upd(v * 2, tl, tm, pos, nv);
+		else upd(v * 2 + 1, tm + 1, tr, pos, nv);
+		segtree[v] = combine(segtree[v * 2], segtree[v * 2 + 1]);
+	}
+}
+
+void rangeupd(int v, int tl, int tr, int l, int r, int add)
+{
+	if (l > r) return;
+	if (l == tl && r == tr) segtree[v] += add;
+	else
+	{
+		int tm = (tl + tr) / 2;
+		rangeupd(v * 2, tl, tm, l, min(r, tm), add);
+		rangeupd(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r, add);
+	}
+}
+
+int a[maxN];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	//ifstream fin("test_input.in");
+
+	cin >> N >> Q;
+
+	for (int i = 1; i <= N; i++)
+		cin >> a[i];
+
+	build(a, 1, 1, N);
+	
+	int t, x, y, z;
+	while (Q--)
+	{
+		cin >> t;
+
+		if (t == 2)
+		{
+			cin >> x;
+			auto k = get_pt(1, 1, N, x);
+			cout << k << "\n";
+		}
+		else
+		{
+			cin >> x >> y >> z;
+			rangeupd(1, 1, N, x, y, z);
+		}
+	}
+
+	return 0;
+}
